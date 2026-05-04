89f28c4f775e18a16efb4a551f20db5d543d9777
Sethmk21 <Kendricks2257@my.uwstout.edu>
2026-05-04 16:44:12 -0500

    they work yippe

--- portal.gd
+++ portal.gd
@@ -2,13 +2,12 @@ extends Node3D
 
 @export var otherPortal: Node3D
 @export var cameraToMove : Camera3D
+@export var camera : Camera3D
 @export var viewport : SubViewport
 @export var PortalVisual : CSGBox3D
 @export var isRot: bool
+@export var startRot: float
 
-func _ready() -> void:
-	if self.rotation.y == 180:
-		isRot = true
 
 func _process(delta):
 
@@ -19,13 +18,20 @@ func update_camera_to_other_portal():
 	#print(curentCamera.global_position)
 	if not curentCamera:
 		return
-	var CC_rel_transform_to_portal = self.global_transform.affine_inverse() * curentCamera.global_transform
+		
+	print("Original camera: " ,self)
+	print(curentCamera.global_transform)
 	
+	var CC_rel_transform_to_portal = self.global_transform.affine_inverse() * curentCamera.global_transform
+	print("Camera space to portal space: " ,self)
+	print(CC_rel_transform_to_portal)
 	var moved_to_other_portal = otherPortal.global_transform * CC_rel_transform_to_portal
+	print("Portal cam to world space: " ,self)
+	print(moved_to_other_portal)
 	cameraToMove.global_transform = moved_to_other_portal
 
-	cameraToMove.rotation = Vector3(0,-cameraToMove.rotation.y + self.rotation.y, 0)
-	cameraToMove.cull_mask = curentCamera.cull_mask
+	#cameraToMove.rotation.y = tan(cameraToMove.rotation.y)
+	#cameraToMove.cull_mask = curentCamera.cull_mask
 	#cameraToMove.set_cull_mask_value(otherPortal.cull_layer, false)
 	
 	viewport.size = get_viewport().get_visible_rect().size
