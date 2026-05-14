7f63edb4c200733e1eb93f819ee520138664e736
Sethmk21 <Kendricks2257@my.uwstout.edu>
2026-05-14 00:42:13 -0500

    Clean up

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -18,22 +18,10 @@ func update_camera_to_other_portal():
 	#print(curentCamera.global_position)
 	if not curentCamera:
 		return
-		
-	#print("Original camera: " ,self)
-	#print(curentCamera.global_transform)
 	
 	var CC_rel_transform_to_portal = self.global_transform.affine_inverse() * curentCamera.global_transform
-	#print("Camera space to portal space: " ,self)
-	#print(CC_rel_transform_to_portal)
 	var moved_to_other_portal = otherPortal.global_transform * CC_rel_transform_to_portal
-	#print("Portal cam to world space: " ,self)
-	#print(moved_to_other_portal)
 	cameraToMove.global_transform = moved_to_other_portal
-
-	#cameraToMove.rotation.y = tan(cameraToMove.rotation.y)
-	#cameraToMove.cull_mask = curentCamera.cull_mask
-	#cameraToMove.set_cull_mask_value(otherPortal.cull_layer, false)
-	
 	viewport.size = get_viewport().get_visible_rect().size
 	viewport.msaa_3d = get_viewport().msaa_3d
 	viewport.screen_space_aa = get_viewport().screen_space_aa
@@ -41,11 +29,7 @@ func update_camera_to_other_portal():
 	viewport.use_debanding = get_viewport().use_debanding
 	viewport.use_occlusion_culling = get_viewport().use_occlusion_culling
 	viewport.mesh_lod_threshold = get_viewport().mesh_lod_threshold
-	#if self.rotation.y >= 0:
-		#cameraToMove.rotation = Vector3(0,0, 0)
-	#
-	#else:
-		#cameraToMove.rotation = Vector3(0,0, 0)
+
 	
 	
 	
